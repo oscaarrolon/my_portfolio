0e83c861cded369f85ef409bca3a433a06f53d77
Oscar Rolón <oscarpcpi2gmai@gmail.com>
2023-01-04 02:19:31 +0100

    Creacion de componente inicio y su correspondiente maquetacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
